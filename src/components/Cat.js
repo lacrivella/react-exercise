9ca9bf41ac818838638bcc8dec965f43afcfff0e
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-14 14:16:17 -0700

    created Cat component

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,2 +1,24 @@
 import React from 'react';
-import 
+import PropTypes from 'prop-types';
+
+function Cat({ name, breed, color }) {
+  return (
+    <dl>
+      <dt>Name</dt>
+      <dd>{name}</dd>
+
+      <dt>Breed</dt>
+      <dd>{breed ? breed : 'unknown'}</dd>
+
+      <dt>Fur Color</dt>
+      <dd>{color}</dd>
+    </dl>
+  );
+}
+
+Cat.propTypes = {
+  name: PropTypes.string.isRequired,
+  breed: PropTypes.string,
+  color: PropTypes.string.isRequired
+};
+export default Cat;
